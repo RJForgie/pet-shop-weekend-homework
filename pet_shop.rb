82a869c98798a251fc10508f3c07814d4aa9877c
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-08-07 10:06:22 +0100

    Tidy up elsif statement after Harrison feedback

--- pet_shop.rb
+++ pet_shop.rb
@@ -87,7 +87,7 @@ end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
   if pet == nil || customer[:cash] < pet[:price] then puts "Sorry"
-  elsif
+  else
       customer[:pets].push(pet)
       pet_shop[:admin][:pets_sold] += 1
       pet_shop[:admin][:total_cash] += pet[:price]
